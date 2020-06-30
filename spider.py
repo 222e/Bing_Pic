a3319c0c5ad9ad588f2a148e5caef61aa5581cd5
222e <62142559+222e@users.noreply.github.com>
2020-06-30 11:48:28 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -6,12 +6,12 @@ def GetPic():
     api = get(api_url)
     json_data = json.loads(api.text)
     pic_url = r'https://www.bing.com{0}'.format(json_data['images'][0]['url'])
-    start_date = json_data['images'][0]['startdate']
-    open(r'./json/{0}.json'.format(start_date), 'wb').write(api.content)
+    start_date = json_data['images'][0]['enddate']
+    open(r'./json/{0}.json'.format(end_date), 'wb').write(api.content)
     print('Create Json Success!')
     pic = get(pic_url, stream=True)
     if(pic.status_code == 200):
-        open(r'./pic/{0}.png'.format(start_date), 'wb').write(pic.content)
+        open(r'./pic/{0}.png'.format(end_date), 'wb').write(pic.content)
         print('Create Image Success!')
     else:
         print('Create Image Faild!')
